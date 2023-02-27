fb29936f6dab35862c4574a04a68400273c98e2a
Carl Egonu <egonucarl@gmail.com>
2023-02-27 18:07:17 +0000

    before npm i jquery jsdom

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+
+app.get('/', (req,res) =>{
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/ggst', (req,res) =>{
+    res.sendFile(__dirname + '/ggst/index.html');
+});
+
+app.listen(port,() =>{
+    console.log('KnowledgeCheck listening on port ' + port)
+})
+
